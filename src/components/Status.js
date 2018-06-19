a97e5fbdbbc40af9ce67e828d0f74db16f062070
kseastman <kat.eastman@gmail.com>
2018-06-19 14:49:46 -0700

    added a status component

---
+++ src/components/Status.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Alert extends Component {
+  static propTypes = {
+    message: PropTypes.string,
+    type: PropTypes.string
+  }
+  render () {
+    return (
+      <section className={`validation-errors-display ${this.props.type}` }>
+        {this.props.type === 'error' ? "There was a problem :" : ""}
+        {this.props.message}
+      </section>
+    )
+  }
+}
