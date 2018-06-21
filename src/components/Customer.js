c29d72ead537870bb9c62ffc07cfb3e6eb1bbefb
Tor Shimizu <torshimizu@gmail.com>
2018-06-21 09:10:46 -0700

    minor refactoring of customer

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -7,31 +7,37 @@ import './Customer.css';
 
 const Customer = (props) => {
     const customer = props.customerData;
-// <img src={movie.image_url} alt={movie.title} />
+
+    let movieList = null;
+    if (customer.checked_out_movies.length === 0) {
+      movieList = "No rentals at this time";
+    } else {
+      movieList = (customer.checked_out_movies.map((movie, index) => {
+        const checkinClosure = () => {
+          props.checkinClick(movie, customer)
+        }
+
+        const imageStyles = {
+          backgroundImage: `url(${movie.image_url})`,
+          backgroundSize: 'cover',
+        }
+
+        return (
+          <li className="customer-movie__image" key={index} style={imageStyles}>
+            <button onClick={checkinClosure}>Checkin Movie</button>
+          </li>
+        )
+      }));
+    }
     return (
       <div className="customer" >
-        <div>
+        <div className="customer-info__items">
           <h3>{customer.name}</h3>
           <p>{customer.phone}</p>
           <button onClick={props.onCustomerCallback}>Select for Rental</button>
         </div>
         <ul className="customer-movie__list">
-          {customer.checked_out_movies.map((movie, index) => {
-            const checkinClosure = () => {
-              props.checkinClick(movie, customer)
-            }
-
-            const imageStyles = {
-              backgroundImage: `url(${movie.image_url})`,
-              backgroundSize: 'cover',
-            }
-
-            return (
-              <li className="customer-movie__image" key={index} style={imageStyles}>
-                <button onClick={checkinClosure}>Checkin Movie</button>
-              </li>
-            )
-          })}
+          {movieList}
         </ul>
       </div>
     );
